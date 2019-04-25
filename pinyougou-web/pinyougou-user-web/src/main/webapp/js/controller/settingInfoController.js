cd409a3a7c481eb480f7aeaa4c13efae78d50356
lzc <keima@itheima.com>
2019-04-25 20:34:20 +0800

    lzc 更新 2019.4.25 用户中心【个人信息】 前端数据绑定

--- pinyougou-web/pinyougou-user-web/src/main/webapp/js/controller/settingInfoController.js
+++ pinyougou-web/pinyougou-user-web/src/main/webapp/js/controller/settingInfoController.js
@@ -5,7 +5,7 @@ app.controller('settingInfoController', function($scope, baseService){
     $scope.update = function () {
         baseService.sendGet("/info/update", $scope.user).then(
             function (response) {
-                $scope.
+                
         });
     }
 
