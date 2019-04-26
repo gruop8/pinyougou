9574519b9b9a47c29c1f5add0fbcaa75472ec6e2
zx <1574408565@qq.com>
2019-04-26 20:21:24 +0800

    zx 结算

--- pinyougou-web/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou-web/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -42,4 +42,105 @@ app.controller('cartController', function ($scope, $controller, baseService) {
             }
         });
     };
+
+
+
+    //选择全部结算的商品
+    $scope.selectAll = function () {
+        if ($scope.allCheck) {
+            angular.forEach($scope.carts, function (cart) {
+                cart.checked = true;
+                angular.forEach(cart.orderItems, function (innerItem) {
+                    innerItem.checked = true;
+                });
+            });
+            $scope.select.selectNum = $scope.totalEntity.totalNum;
+            $scope.select.selectMoney = $scope.totalEntity.totalMoney;
+        } else {
+            angular.forEach($scope.carts, function (cart) {
+                cart.checked = false;
+                angular.forEach(cart.orderItems, function (innerItem) {
+                    innerItem.checked = false;
+                })
+            });
+            $scope.select.selectNum = 0;
+            $scope.select.selectMoney = 0;
+        }
+    };
+
+    //选择一个商家的商品
+    $scope.selectCarts = function (cart, carts) {
+        if (cart.checked) {
+
+            for(var i = 0; i < cart.orderItems.length; i++){
+                $scope.select.selectNum += cart.orderItems[i].num;
+                $scope.select.selectMoney += cart.orderItems[i].totalFee;
+            }
+
+            var flag = true;
+            angular.forEach(cart.orderItems, function (innerItem) {
+                innerItem.checked = true;
+            });
+            angular.forEach(carts, function (outerItem) {
+                if (outerItem.checked == false || typeof(outerItem.checked) == "undefined") {
+                    flag = false;
+                }
+            });
+
+            if(flag)
+            {
+                $scope.allCheck = true;
+            }
+        } else {
+            for(var i = 0; i < cart.orderItems.length; i++){
+                $scope.select.selectNum -= cart.orderItems[i].num;
+                $scope.select.selectMoney -= cart.orderItems[i].totalFee;
+            }
+
+            $scope.allCheck = false;
+            angular.forEach(cart.orderItems, function (innerItem) {
+                innerItem.checked = false;
+            });
+
+        }
+    };
+
+
+    //选中商品总金额
+    $scope.select = {selectNum : 0, selectMoney : 0};
+
+    //选中一个商品
+    $scope.selectOne = function ( orderItem, orderCart, cart, carts ) {
+
+        var flag = true;
+        if (orderItem.checked) {
+            // 统计购买数量
+            $scope.select.selectNum += orderItem.num;
+            // 统计总金额
+            $scope.select.selectMoney += orderItem.totalFee;
+            angular.forEach(orderCart, function (innerItem) {
+                if (innerItem.checked == false || typeof(innerItem.checked) == "undefined") {
+                    flag = false;
+                }
+            });
+        }
+        else {
+            $scope.select.selectNum -= orderItem.num;
+            $scope.select.selectMoney -= orderItem.totalFee;
+            $scope.allCheck = false;
+            cart.checked = false;
+            flag = false;
+        }
+        if (flag) {
+            cart.checked = true;
+            angular.forEach(carts, function (outerItem) {
+                if (outerItem.checked == false || typeof(outerItem.checked) == "undefined") {
+                    flag = false;
+                }
+            });
+        }
+        if (flag) {
+            $scope.allCheck = true;
+        }
+    };
 });
